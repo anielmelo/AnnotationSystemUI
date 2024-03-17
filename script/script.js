9c39a0a9cdd0ef5319ec98487f74faa492cb08fb
anielmelo <anielcontato@gmail.com>
2024-03-17 14:34:44 -0300

    update (login and annotations)

--- script/script.js
+++ script/script.js
@@ -1,13 +1,17 @@
-const buttonLogin = document.querySelector('#tab-login');
-const buttonRegister = document.querySelector('#tab-register');
+const tabLogin = document.querySelector('#tab-login');
+const tabRegister = document.querySelector('#tab-register');
+
+const login = document.querySelector('#login-form');
+const register = document.querySelector('#register-form');
+
 const form = document.querySelector('#account-access');
 const loginVisible = document.querySelector('#login-visibility');
 const registerVisible = document.querySelector('#register-visibility');
 
-buttonLogin.addEventListener('click', () => {
+tabLogin.addEventListener('click', () => {
     // altera o botão tag
-    buttonLogin.className = 'active'
-    buttonRegister.className  = ''
+    tabLogin.className = 'active'
+    tabRegister.className  = ''
     // altera a cor de fundo
     form.className = ''
     form.classList.add('login')
@@ -16,14 +20,24 @@ buttonLogin.addEventListener('click', () => {
     registerVisible.className = 'form-visibility-false'
 })
 
-buttonRegister.addEventListener('click', () => {
+tabRegister.addEventListener('click', () => {
     // altera o botão tag
-    buttonRegister.className  = 'active'
-    buttonLogin.className = ''
+    tabRegister.className  = 'active'
+    tabLogin.className = ''
     // altera a cor de fundo
     form.className = ''
     form.classList.add('register')
     // altera a visibilidade
     registerVisible.className = 'form-visibility-true'
     loginVisible.className = 'form-visibility-false'
+})
+
+login.addEventListener('submit', (event) => {
+    event.preventDefault()
+    window.location.href = 'annotations.html'
+})
+
+register.addEventListener('submit', (event) => {
+    event.preventDefault()
+    window.location.href = 'annotations.html'
 })
