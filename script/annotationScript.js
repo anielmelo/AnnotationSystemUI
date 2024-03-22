a7416cd6205461b09b2422aa3f636ba0ba6e70e8
anielmelo <anielcontato@gmail.com>
2024-03-22 14:46:35 -0300

    update style (annotation, login)

--- script/annotationScript.js
+++ script/annotationScript.js
@@ -1,5 +1,12 @@
 const description = document.querySelectorAll('.description')
-const textDescription = document.querySelector('#text-description')
+
+const buttonCreate = document.querySelector('#btn-create')
+const buttonEdit = document.querySelector('#btn-note-edit') // review
+const buttonClose = document.querySelector('#btn-close-card')
+
+const coverCard = document.querySelector('.uncover-card')
+const cardAdd = document.querySelector('.card-add-disabled')
+const cardEdit = document.querySelector('.card-edit-disabled') // review
 
 function textHidden(element, limitNumber) {
     let text = element.textContent
@@ -13,4 +20,19 @@ function textHidden(element, limitNumber) {
 description.forEach((div) => {
     let textDescription = div.querySelector('#text-description')
     textHidden(textDescription, 90)
+})
+
+buttonCreate.addEventListener('click', () => {
+    coverCard.className = 'cover-card'
+    cardAdd.className = 'card-add'
+})
+
+buttonEdit.addEventListener('click', () => {
+    coverCard.className = 'cover-card'
+    cardEdit.className = 'card-edit'
+})
+
+buttonClose.addEventListener('click', () => {
+    coverCard.className = 'uncover-card'
+    cardEdit.className = 'card-add-disabled'
 })
